98fefa1955a7772693e24fd739719c439dcbd7f6
Hindrik Bruinsma <de@readefries.nl>
2017-04-21 23:14:00 +0200

    Added some examples to the playground

--- Example/RFIBANHelper.playground/Contents.swift
+++ Example/RFIBANHelper.playground/Contents.swift
@@ -2,3 +2,22 @@
 
 import UIKit
 import RFIBANHelper
+
+let sut = "GR160110125000000001230065"
+
+var result = RFIBANHelper.isValidIBAN(sut)
+
+let no1 = "NO9370580671290"
+
+result = RFIBANHelper.isValidIBAN(no1)
+
+let no2 = "NO9350050669316"
+
+result = RFIBANHelper.isValidIBAN(no2)
+
+let no3 = "NO9386011117947"
+
+result = RFIBANHelper.isValidIBAN(no3)
+
+
+let iban = RFIBANHelper.createIBAN("70580671290", countryCode: "NO")
