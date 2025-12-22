740472b513b203b5538c36e6f3d9e70eee5b4d04
Hendrik Bruinsma <hbruinsma@xs4some.nl>
2025-12-22 11:13:07 +0100

    Updating countries list for supported IBANs

---
+++ import.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+IN_FILE='iban-countries.json'
+OUT_FILE='RFIBAN-Helper/Assets/IBANStructure.json'
+
+if [ ! -f "$IN_FILE" ]; then
+echo "Unable to read file $IN_FILE"
+exit 1
+fi
+
+jq 'map({ (. [0]): { CountryCode: .[0], Length: .[1], InnerStructure: .[2], Example: .[3], Required: (. [4] == "y"), SEPA: (. [5] == "y"), "EU924-2009": (. [6] == "y"), EUR: (. [7] == "y") } }) | add' "$IN_FILE" | jq --compact-output > "$OUT_FILE"
+
+if [ $? -ne 0 ]; then
+echo "Unable to process JSON. Is the file contents valid JSON?"
+exit 1
+fi
+
+echo "Transformation complete. Output written to $OUT_FILE"
